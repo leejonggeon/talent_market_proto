b707fa7474064b7d784c5fb894f9a9313f14bae5
bluewink <59544716+bluewink@users.noreply.github.com>
2020-09-14 18:48:20 +0900

    revised after merge with j1

--- client/src/components/views/StatusBar/StatusBar.js
+++ client/src/components/views/StatusBar/StatusBar.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../css/styles.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
-
+import { auth } from "../../../_action/user_action";
+import { useDispatch } from "react-redux";
 
 function StatusBar(props) {
-  const [Login_flag, setLogin_flag] = useState(true);
+  const dispatch = useDispatch();
   //Login_flag 0 : 로그아웃 1 : 로그인
+  const [Login_flag, setLogin_flag] = useState(true);
+  useEffect(() => {
+    //백엔드에서 가져온 정보가 response에 들어 있음.
+    dispatch(auth()).then((response) => {
+      //console.log(response);
+
+      //로그인 하지 않은 상태
+      if (!response.payload.isAuth) {
+        setLogin_flag(false);
+      } else {
+        //로그인한 상태
+        setLogin_flag(true);
+      }
+    });
+  }, []);
+
   const onClickHandler = () => {
     axios.get("/api/users/logout").then((response) => {
       if (response.data.success) {
@@ -37,17 +54,16 @@ function StatusBar(props) {
               </div>
               <div className="status-bar__column">
                 <div className="status-bar__button login">
-                  <Link className="link" to="/login">
-                    로그인
-                  </Link>
-                  {/* {Login_flag ? (
+                  {Login_flag ? (
                     //로그인 되어 있으면
-                    <a onClick={onClickHandler}>로그아웃</a>
+                    <a className="link" onClick={onClickHandler}>
+                      로그아웃
+                    </a>
                   ) : (
                     <Link className="link" to="/login">
                       로그인
                     </Link>
-                  )} */}
+                  )}
                   {/* <a onClick={onClickHandler}>로그인</a> */}
                 </div>
                 <div className="status-bar__button signup">
@@ -56,10 +72,16 @@ function StatusBar(props) {
                   </Link>
                 </div>
                 <div className="status-bar__button profile">
-                  <a href="profile"> 나의 프로필</a>
+                  <Link className="link profile" to="/profile">
+                    {" "}
+                    나의 프로필
+                  </Link>
                 </div>
                 <div className="status-bar__button course_board">
-                  <a href="course_board"> 게시판</a>
+                  <Link className="link course_board" to="/course_board">
+                    {" "}
+                    게시판
+                  </Link>
                 </div>
               </div>
             </div>
@@ -70,4 +92,4 @@ function StatusBar(props) {
   );
 }
 
-export default StatusBar;
+export default withRouter(StatusBar);
